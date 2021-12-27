95790551810cf5da307ffe92bf33da2e0158ebc9
Andrew <andrew.warnock@outlook.com>
2021-12-27 18:08:24 +0000

    Implemented XML file storage and dynamic directory
    instead of text files, changed it to use XML, and kept months inside the same one file instead of several files

--- Calorie budget/Form1.cs
+++ Calorie budget/Form1.cs
@@ -8,50 +8,129 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using Newtonsoft.Json;
-using System.Web.Script.Serialization;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace Calorie_budget
 {
     public partial class Form1 : Form
     {
-        int deficet= 0;
         DateTime now;
-        string path;
         CalorieData data;
+        string[] months = {"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
 
         public Form1()
         {
             InitializeComponent();
-            __LoadFile__();
-            __LoadData__();
             __initialize__();
+            //__LoadFile__();
+            __LoadData__();
+
         }
 
         private void __initialize__()
         {
-            data  = new CalorieData(this.now, this.deficet);
-            string path = $"C:\\Users\\Andre\\OneDrive\\Documents\\WeightCalculator\\Calorie-Count-{this.now.Month}-{this.now.Year}\\{this.now.Day}.json";
+            this.now = DateTime.Now;
 
-            //string json = JsonConvert.SerializeObject(data);
-            string json = File.ReadAllText(path);
+            this.data  = new CalorieData(this.now, 0);
+            string path = Directory.GetCurrentDirectory();
+            path += $"\\Storage\\Calorie-Count-{months[this.now.Month]}-{this.now.Year}.xml";
 
-            JavaScriptSerializer ser = new JavaScriptSerializer();
-            var personlist = ser.Deserialize<List<CalorieData>>(json);
+            if (File.Exists(path))
+            {
+                if (!loadFromXML(path))
+                {
+                    addtoXML(0, this.now.Day, path);
+                }
+            }
+            else
+            {
+                createXML(path);
+            }
 
-            Console.WriteLine(personlist);
         }
 
-        private void __LoadFile__()
+        private void createXML(string path)
+        {
+            XmlWriter xmlWriter = XmlWriter.Create(path);
+
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement(months[this.now.Month]);
+
+            xmlWriter.WriteStartElement("caloriesdata");
+            xmlWriter.WriteAttributeString("calories", this.data.calories.ToString());
+            xmlWriter.WriteAttributeString("day", this.data.date.Day.ToString());
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Close();
+        }
+
+        private void addtoXML(int calories, int day, string path)
+        {
+            XDocument xDocument = XDocument.Load(path);
+
+            XElement root = xDocument.Element(months[this.now.Month]);
+            IEnumerable<XElement> rows = root.Descendants("caloriesdata");
+            XElement lastRow = rows.Last();
+            lastRow.AddAfterSelf(
+               new XElement("caloriesdata",
+               new XAttribute("calories", calories),
+               new XAttribute("day", day)));
+            xDocument.Save(path);
+        }
+
+        private void updateXML(int calories, int day, string path)
+        {
+
+            int comp;
+            XDocument xDocument = XDocument.Load(path);
+
+            foreach (XElement xe in xDocument.Elements().Elements())
+            {
+                if(int.TryParse(xe.Attribute("day").Value, out comp))
+                {
+                    if (comp == day)
+                    {
+                        xe.Attribute("calories").SetValue(calories.ToString());
+                    }
+                }
+            }
+            xDocument.Save(path);
+            __LoadData__();
+        }
+
+        private bool loadFromXML(string path)
+        {
+            string xmldoc = File.ReadAllText(path);
+
+            XmlDocument xmlDocument = new XmlDocument();
+
+            xmlDocument.LoadXml(xmldoc);
+
+            foreach(XmlElement xe in xmlDocument.DocumentElement )
+            {
+                int comp = int.Parse(xe.GetAttribute("day"));
+                if (comp == this.now.Day)
+                {
+                    this.data.calories = int.Parse(xe.GetAttribute("calories"));
+                    __LoadData__();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /* private void __LoadFile__()
         {
             this.now = DateTime.Now;
-            this.path = $"C:\\Users\\Andre\\OneDrive\\Documents\\WeightCalculator\\Calorie-Count-{this.now.Month}-{this.now.Year}\\{this.now.Day}.txt";
+            this.path = $"C:\\Users\\Documents\\WeightCalculator\\Calorie-Count-{this.now.Month}-{this.now.Year}\\{this.now.Day}.txt";
             lblNotification.Text = "";
 
-            if(!Directory.Exists($"C:\\Users\\Andre\\OneDrive\\Documents\\WeightCalculator\\Calorie-Count-{this.now.Month}-{this.now.Year}"))
+            if(!Directory.Exists($"C:\\OneDrive\\Documents\\WeightCalculator\\Calorie-Count-{this.now.Month}-{this.now.Year}"))
             {
-                Directory.CreateDirectory($"C:\\Users\\Andre\\OneDrive\\Documents\\WeightCalculator\\Calorie-Count-{this.now.Month}-{this.now.Year}");
+                Directory.CreateDirectory($"C:\\OneDrive\\Documents\\WeightCalculator\\Calorie-Count-{this.now.Month}-{this.now.Year}");
             }
 
             if (!File.Exists(this.path))
@@ -82,22 +161,23 @@ namespace Calorie_budget
                 //txtDisplay.Text = text;
             }
         }
+        */
 
         private void __LoadData__()
         {
-            int calorieBalanece = 1961 - this.deficet;
+            int calorieBalanece = 1961 - this.data.calories;
 
             lblCalorieBalance.Text = $"Calorie Balance: {calorieBalanece}cal";
         }
 
-        private void __WriteData__()
+        /*private void __WriteData__()
         {
             using (StreamWriter sw = File.CreateText(this.path))
             {
                 sw.WriteLine($"Calorie counter {this.now.Month}-{this.now.Year}");
                 sw.WriteLine(deficet.ToString());
             }
-        }
+        }*/
 
         private void __ReadData__(string selectedPath)
         {
@@ -123,9 +203,9 @@ namespace Calorie_budget
 
             lblNotification.Text = $"You have added {item}";
 
-            this.deficet += calorie;
+            this.data.calories += calorie;
             __LoadData__();
-            __WriteData__();
+            //__WriteData__();
 
 
         }
@@ -137,11 +217,14 @@ namespace Calorie_budget
         private void btnAddCalories_Click(object sender, EventArgs e)
         {
             int deduct = 0;
-            if(int.TryParse(txtAddDeficet.Text.Trim(), out deduct))
+            string path = Directory.GetCurrentDirectory();
+            path += $"\\Storage\\Calorie-Count-{months[this.now.Month]}-{this.now.Year}.xml";
+
+            if (int.TryParse(txtAddDeficet.Text.Trim(), out deduct))
             {
-                this.deficet += deduct;
+                this.data.calories += deduct;
                 __LoadData__();
-                __WriteData__();
+                updateXML(this.data.calories, this.now.Day, path);
             }
             else
             {
@@ -151,7 +234,8 @@ namespace Calorie_budget
 
         private void btnLibraryAdd_Click(object sender, EventArgs e)
         {
-            string path = @"C:\Users\Andre\OneDrive\Documents\WeightCalculator\Food-Library\";
+            string path = Directory.GetCurrentDirectory();
+            path += @"\Storage\Food-Library\";
 
             OpenFileDialog library = new OpenFileDialog();
             library.InitialDirectory = path;
